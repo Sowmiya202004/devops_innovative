f7d30e73177423180cf772c98a8e4bdc76961a19
Sowmiya202004 <sunsowmiya20@gmail.com>
2026-03-14 21:52:02 +0530

    Implement Course Management Server with CRUD operations

--- src/main/java/com/devops/CourseManagementServer.java
+++ src/main/java/com/devops/CourseManagementServer.java
@@ -1,60 +1,146 @@
-package com.devops;
+import com.sun.net.httpserver.*;
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.util.*;
 
-import java.applet.Applet;
-import java.awt.*;
-import java.awt.event.*;
+class Course {
+    int id;
+    String name;
+    String instructor;
 
-public class CourseManagementApplet extends Applet implements ActionListener {
+    Course(int id, String name, String instructor) {
+        this.id = id;
+        this.name = name;
+        this.instructor = instructor;
+    }
+}
 
-    Label l1, l2, l3, result;
-    TextField t1, t2, t3;
-    Button addBtn, clearBtn;
+public class CourseManagementServer {
 
-    public void init() {
+    static List<Course> courses = new ArrayList<>();
 
-        l1 = new Label("Course ID:");
-        l2 = new Label("Course Name:");
-        l3 = new Label("Instructor:");
+    public static void main(String[] args) throws Exception {
 
-        t1 = new TextField(15);
-        t2 = new TextField(15);
-        t3 = new TextField(15);
+        HttpServer server = HttpServer.create(new InetSocketAddress(8085), 0);
 
-        addBtn = new Button("Add Course");
-        clearBtn = new Button("Clear");
+        // Home Page
+        server.createContext("/", exchange -> {
 
-        result = new Label("");
+            String html = """
+            <html>
+            <body>
+            <h2>Course Management System</h2>
 
-        add(l1);
-        add(t1);
-        add(l2);
-        add(t2);
-        add(l3);
-        add(t3);
-        add(addBtn);
-        add(clearBtn);
-        add(result);
+            <h3>Add Course</h3>
+            <form action="/add">
+            Course ID: <input name="id"><br><br>
+            Course Name: <input name="name"><br><br>
+            Instructor: <input name="instructor"><br><br>
+            <input type="submit" value="Add Course">
+            </form>
 
-        addBtn.addActionListener(this);
-        clearBtn.addActionListener(this);
+            <br>
+            <a href="/view">View Courses</a>
+
+            <br><br>
+
+            <h3>Delete Course</h3>
+            <form action="/delete">
+            Course ID: <input name="id"><br><br>
+            <input type="submit" value="Delete Course">
+            </form>
+
+            </body>
+            </html>
+            """;
+
+            exchange.getResponseHeaders().set("Content-Type","text/html");
+            exchange.sendResponseHeaders(200, html.length());
+
+            OutputStream os = exchange.getResponseBody();
+            os.write(html.getBytes());
+            os.close();
+        });
+
+        // Add Course
+        server.createContext("/add", exchange -> {
+
+            Map<String,String> params = parseQuery(exchange.getRequestURI().getQuery());
+
+            int id = Integer.parseInt(params.get("id"));
+            String name = params.get("name");
+            String instructor = params.get("instructor");
+
+            courses.add(new Course(id,name,instructor));
+
+            String response = "<html><body><h3>Course Added</h3><a href='/'>Go Back</a></body></html>";
+
+            exchange.getResponseHeaders().set("Content-Type","text/html");
+            exchange.sendResponseHeaders(200,response.length());
+
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        });
+
+        // View Courses
+        server.createContext("/view", exchange -> {
+
+            StringBuilder html = new StringBuilder();
+            html.append("<html><body><h2>Course List</h2>");
+
+            for(Course c : courses){
+                html.append("ID: ").append(c.id)
+                    .append(" | Name: ").append(c.name)
+                    .append(" | Instructor: ").append(c.instructor)
+                    .append("<br>");
+            }
+
+            html.append("<br><a href='/'>Go Back</a></body></html>");
+
+            exchange.getResponseHeaders().set("Content-Type","text/html");
+            exchange.sendResponseHeaders(200, html.length());
+
+            OutputStream os = exchange.getResponseBody();
+            os.write(html.toString().getBytes());
+            os.close();
+        });
+
+        // Delete Course
+        server.createContext("/delete", exchange -> {
+
+            Map<String,String> params = parseQuery(exchange.getRequestURI().getQuery());
+
+            int id = Integer.parseInt(params.get("id"));
+
+            courses.removeIf(c -> c.id == id);
+
+            String response = "<html><body><h3>Course Deleted</h3><a href='/'>Go Back</a></body></html>";
+
+            exchange.getResponseHeaders().set("Content-Type","text/html");
+            exchange.sendResponseHeaders(200,response.length());
+
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        });
+
+        server.start();
+
+        System.out.println("Server started at http://localhost:8085");
     }
 
-    public void actionPerformed(ActionEvent e) {
+    static Map<String,String> parseQuery(String query){
 
-        if (e.getSource() == addBtn) {
+        Map<String,String> map = new HashMap<>();
 
-            String id = t1.getText();
-            String name = t2.getText();
-            String instructor = t3.getText();
+        if(query == null) return map;
 
-            result.setText("Course Added: " + name + " (Instructor: " + instructor + ")");
+        for(String param : query.split("&")){
+            String[] pair = param.split("=");
+            map.put(pair[0], pair[1]);
         }
 
-        if (e.getSource() == clearBtn) {
-            t1.setText("");
-            t2.setText("");
-            t3.setText("");
-            result.setText("");
-        }
+        return map;
     }
 }
